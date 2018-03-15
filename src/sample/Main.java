f967af0f1f4dc4be6bb5f451349b1560d5093ad5
Katarinaphox <Katarinaphox@mail.ru>
2018-03-15 17:38:15 +0300

    homework

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,23 +1,50 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private ObservableList<Book> book = FXCollections.observableArrayList();
 
+    public Main() {
+        System.out.println("Main constructor");
+
+       // book.add(new Book());
+       // book.add(new Book());
+       // book.add(new Book());
+    }
+
+    public ObservableList<Book> getBook() {
+        return book;
+    }
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("libr.fxml"));
-        Scene scene = new Scene(root, 600, 400);
+       // Parent root = FXMLLoader.load(getClass().getResource("libr2.fxml"));
+        System.out.println("Application start");
+
+        FXMLLoader loader=new FXMLLoader() ;
+        loader.setLocation(getClass().getResource("libr2.fxml"));
+        AnchorPane root=loader.load();
+        SampleController controller=loader.getController();
+
+        primaryStage.setScene(new Scene(root));;
         scene.getStylesheets().add(0, "styles/my.css");
-        primaryStage.setScene(scene);
         primaryStage.show();
+
+        //PersonOverviewController controller = loader.getController();
+       // controller.setMainApp(this);
+
     }
 
     public static void main(String[] args) {
+
         launch(args);
     }
+    // System.out.println("Application close");
 }
